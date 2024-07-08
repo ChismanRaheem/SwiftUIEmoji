de72fa2ea57328d125e8cf6a40a48a8d6199fbc0
Raheem <rchisman@microsoft.com>
2024-07-08 15:46:50 -0700

    confirmed

--- SwiftUIEmoji/ContentView.swift
+++ SwiftUIEmoji/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Emoji: String, CaseIterable{
+        case 😄, 😎,🤩,👻,🥦,🐝
+    }
+    @State var myEmoji: Emoji = .🐝
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //Image(systemName: "globe")
+              //  .imageScale(.large)
+               // .foregroundStyle(.tint)
+            //Text("Hello, world!")
+            Text(myEmoji.rawValue).font(.system(size: 150)).padding()
+            
+            Button("Click Me") {
+                myEmoji = Emoji.allCases.randomElement()!
+            }
         }
         .padding()
     }
