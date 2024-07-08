3f6ad2fb113d5fa80b83bc90780d4825a5dfe56e
Raheem Chisman <raheem.chisman@microsoft.com>
2024-07-07 21:09:40 -0700

    Initial Commit

---
+++ SwiftUIEmoji/SwiftUIEmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEmojiApp.swift
+//  SwiftUIEmoji
+//
+//  Created by Raheem Chisman on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
